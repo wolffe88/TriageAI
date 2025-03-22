af419d7c4a686ad849a01904bfa24a53a9d4efca
lay <ccev3628@nus.edu.sg>
2025-03-22 16:20:40 +0800

    added tts

--- TriageAI/TriageAI/settings.py
+++ TriageAI/TriageAI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY", "sk_540e9615dbddf0bf50ce7c322b0514a4704393320aa85edd")
